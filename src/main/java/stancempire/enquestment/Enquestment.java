fba8eb7a531458d7772cebe5a9534b4ddc771d16
StanCEmpire <stancempire@gmail.com>
2023-11-14 21:29:03 +1100

    Add mod setup methods

--- src/main/java/stancempire/enquestment/Enquestment.java
+++ src/main/java/stancempire/enquestment/Enquestment.java
@@ -1,8 +1,26 @@
 package stancempire.enquestment;
 
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+
+@Mod(Enquestment.MOD_ID)
 public class Enquestment
 {
 
-	
-	
+    public static final String MOD_ID = "enquestment";
+
+    public Enquestment()
+    {
+
+
+
+    }
+
+    public void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+
+
+    }
+
 }
---
+++ src/main/java/stancempire/enquestment/client/ClientSetup.java
@@ -0,0 +1,15 @@
+package stancempire.enquestment.client;
+
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ClientSetup
+{
+
+    public static void clientSetup(final FMLClientSetupEvent event)
+    {
+
+
+
+    }
+
+}
